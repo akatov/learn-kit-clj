b680a0b4ef8ac94c7270dc1e7b6b4617a46ff689
Dmitri Akatov <dmitri@akatov.com>
2022-08-08 20:14:22 +0200

    introduce some state

--- src/clj/akatov/guestbook/web/controllers/health.clj
+++ src/clj/akatov/guestbook/web/controllers/health.clj
@@ -1,13 +1,19 @@
 (ns akatov.guestbook.web.controllers.health
   (:require
-    [ring.util.http-response :as http-response])
+   [clojure.tools.logging :as log]
+   [ring.util.http-response :as http-response])
   (:import
-    [java.util Date]))
+   [java.util Date]))
+
+(defonce num-requests (atom 0))
 
 (defn healthcheck!
   [req]
+  (swap! num-requests + 1)
+  (log/info (str "and another request. total requests: " @num-requests))
   (http-response/ok
-    {:time     (str (Date. (System/currentTimeMillis)))
-     :up-since (str (Date. (.getStartTime (java.lang.management.ManagementFactory/getRuntimeMXBean))))
-     :app      {:status  "up"
-                :message ""}}))
+   {:time     (str (Date. (System/currentTimeMillis)))
+    :up-since (str (Date. (.getStartTime (java.lang.management.ManagementFactory/getRuntimeMXBean))))
+    :app      {:status  "up"
+               :message ""
+               :num-requests @num-requests}}))
